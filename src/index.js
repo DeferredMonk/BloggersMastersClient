5d00a02732be9075410917d7530b43509e0334ae
Iida Timperi s318305 <s318305@studenti.polito.it>
2023-05-30 18:02:11 +0200

    fix: fixed keycloak

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import reportWebVitals from "./reportWebVitals"
-import { initialize } from "./keycloak"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { initialize } from "./keycloak";
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const setup = async () => {
-  // try {
-  //   await initialize()
-  //   return root.render(<App />)
-  // } catch (e) {
-  //   return root.render(
-  //     <React.StrictMode>
-  //       <div>Error connecting to keycloak</div>
-  //     </React.StrictMode>
-  //   )
-  // }
-  return root.render(<App />)
-}
-setup()
+  try {
+    await initialize();
+    return root.render(<App />);
+  } catch (e) {
+    return root.render(
+      <React.StrictMode>
+        <div>Error connecting to keycloak</div>
+      </React.StrictMode>
+    );
+  }
+};
+setup();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
